f9cbb03f2f1efdcca1804faac7aaeb2747aa286d
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-12 18:41:23 +0200

    Add morgan middleware

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const itemsRouter = require("./items/itemsRouter");
+const morgan = require('morgan')
+const express = require("express")
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
 app.use("/", itemsRouter);
 
 app.listen(3001, () => console.log("Listening on port 3001"));
