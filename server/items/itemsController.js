a5d63fd26eab218ddf5990cdfbcf37060f539bf2
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-12 18:39:27 +0200

    Set a default value for page

--- server/items/itemsController.js
+++ server/items/itemsController.js
@@ -3,7 +3,7 @@ const { findItems } = require('../functions');
 exports.searchItems = async (req, res) => {
     try {
         const { query } = req.query;
-        const { page } = req.query || 1
+        const { page = 1 } = req.query
         console.log(page);
         const queryString = `https://www.ebay.com/sch/i.html?_from=R40&_nkw=${query}&_sacat=0&_pgn=${page}`
         const items = await findItems(queryString);
