e04c9d5f20f521e5d7888ac30982150843e2a4a4
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-12 19:03:49 +0200

    Fix function response status & json

--- server/items/itemsController.js
+++ server/items/itemsController.js
@@ -2,12 +2,11 @@ const { findItems } = require('../functions');
 
 exports.searchItems = async (req, res) => {
     try {
-        const { query } = req.query;
+        const { query } = req.query;    
         const { page = 1 } = req.query
-        console.log(page);
         const queryString = `https://www.ebay.com/sch/i.html?_from=R40&_nkw=${query}&_sacat=0&_pgn=${page}`
         const items = await findItems(queryString);
-        res.send(items); 
+        res.status(200).json({ success: true, data: items });
     } catch (error) {
         console.error("Error:", error);
         res.status(500).send("Internal Server Error"); // Handle errors
