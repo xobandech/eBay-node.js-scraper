aab493e6d19bd773e32eb900cf0d846f5a5c084c
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-12 21:59:47 +0200

    Make function to use cheerio for parsing data

--- server/functions.js
+++ server/functions.js
@@ -1,31 +1,30 @@
 const axios = require("axios");
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const cheerio = require("cheerio");
 
 exports.findItems = async (link) => {
     const items = [];
 
     try {
         const response = await axios.get(link); // Wait for the GET request to complete
-        const dom = new JSDOM(response.data);
+        const $ = cheerio.load(response.data);
 
-        const titles = [...dom.window.document.querySelectorAll(".s-item__title span")];
-        const prices = [...dom.window.document.querySelectorAll(".s-item__price")];
-        const images = [...dom.window.document.querySelectorAll(".s-item__image-wrapper img")];
-        const links = [...dom.window.document.querySelectorAll(".s-item__link")];
+        const titles = $(".s-item__title span");
+        const prices = $(".s-item__price");
+        const images = $(".s-item__image-wrapper img");
+        const links = $(".s-item__link");
 
         for (let i = 0; i < titles.length; i++) {
-            const   title = titles[i].textContent;
-            const price = prices[i] && prices[i].textContent;
-            const image = images[i] && images[i].getAttribute("src");
-            const ebayLink = links[i] && links[i].getAttribute("href");
+            const title = $(titles[i]).text();
+            const price = $(prices[i]).text();
+            const image = $(images[i]).attr("src");
+            const ebayLink = $(links[i]).attr("href");
 
             if (title !== "Shop on eBay" && price !== undefined && image !== undefined) {
                 items.push({ title, price, image, ebayLink });
             }
         }
 
-        return items
+        return items;
     } catch (error) {
         throw error;
     }
