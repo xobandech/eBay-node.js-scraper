ed4b16aad87428b077c93ef5b96dc9dfd45ae0c3
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-13 11:44:19 +0200

    Add Routes & Route to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import SearchResultPage from "./pages/SearchPage";
+import SearchComponent from "./components/SearchComponent";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-
-    </div>
+      <Routes>
+        <Route path="/" element={<SearchComponent />}>
+          <Route path="search" element={<SearchResultPage />} />
+        </Route>
+      </Routes>
   );
 }
 
