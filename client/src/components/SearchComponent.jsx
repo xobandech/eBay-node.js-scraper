df1e71a0d699948a574a5becb07cf6c5e4ae611f
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-13 12:11:20 +0200

    Add pagination

--- client/src/components/SearchComponent.jsx
+++ client/src/components/SearchComponent.jsx
@@ -3,6 +3,8 @@ import { Outlet } from "react-router-dom";
 
 const SearchComponent = () => {
   const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+
   return (
     <>
       <div className="w-full flex justify-center">
@@ -19,6 +21,32 @@ const SearchComponent = () => {
         </button>
       </div>
       <Outlet />
+      <div className="flex justify-center mb-44">
+        <button
+          onClick={() => {
+            if (page > 1) {
+                const urlParams = new URLSearchParams(window.location.search);
+                const query = urlParams.get('query');
+                const page = urlParams.get('page')
+                window.location.href = `/search?query=${query}&page=${page}`;
+            }
+          }}
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >
+          Previous Page
+        </button>
+        <button
+          onClick={() => {
+            const urlParams = new URLSearchParams(window.location.search);
+            const query = urlParams.get('query');
+            const page = urlParams.get('page')
+            window.location.href = `/search?query=${query}&page=${page}`;
+          }}
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >
+          Next Page
+        </button>
+      </div>
     </>
   );
 };
