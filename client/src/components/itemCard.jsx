8268d519081950c70e36c04e76db928895f63420
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-13 11:46:18 +0200

    Create ItemCard

--- client/src/components/itemCard.jsx
+++ client/src/components/itemCard.jsx
@@ -1,5 +1,17 @@
-const ItemCard = (item) => {
-    
-}
+const ItemCard = ({ item }) => {
+  return (
+    <li className="list-none mb-4">
+      <a href={item.ebayLink} className="flex ">
+        <div className="w-[300px] max-w-[300px] flex justify-center bg-gray-200">
+          <img src={item.image} alt="" />
+        </div>
+        <div>
+          <p>{item.title}</p>
+          <p>{item.price}</p>
+        </div>
+      </a>
+    </li>
+  );
+};
 
-export default ItemCard
+export default ItemCard;
