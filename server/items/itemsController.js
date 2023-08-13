5776ab050047bebd01dfbb20e11f115c89b63697
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-13 11:49:10 +0200

    Fix response message

--- server/items/itemsController.js
+++ server/items/itemsController.js
@@ -2,13 +2,14 @@ const { findItems } = require('../functions');
 
 exports.searchItems = async (req, res) => {
     try {
-        const { query } = req.query;    
+        const { query } = req.query;
         const { page = 1 } = req.query
+        console.log(query, page);
         const queryString = `https://www.ebay.com/sch/i.html?_from=R40&_nkw=${query}&_sacat=0&_pgn=${page}`
         const items = await findItems(queryString);
         res.status(200).json({ success: true, data: items });
     } catch (error) {
         console.error("Error:", error);
-        res.status(500).send("Internal Server Error"); // Handle errors
+        res.status(500).send("Internal Server Error");
     }
 };
